ec081531ee5999095bb7d5c143ec0cd4a15065b4
Maria Estrella <98559654+mlestrella843@users.noreply.github.com>
2024-12-21 10:14:20 -0700

    Setting up the project

---
+++ final-lab/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <div>
+        <h1>Portfolio: Final Lab</h1>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
